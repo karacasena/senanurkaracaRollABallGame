5116a2b1763a70fe3ad370af903a64c0f96eabf2
karacasena <karacasena50@gmail.com>
2026-04-08 16:22:23 +0300

    finished storiel
    I did plane and player

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    public float speed = 6f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // Basit ground check (CharacterController üzerinden)
+        isGrounded = controller.isGrounded;
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // Movement
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(x, 0f, z);
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Jump (SPACE)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Gravity
+        velocity.y += gravity * Time.deltaTime;
+
+        // Apply vertical movement
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # senanurkaraca
 
+RollABallGame - Midterm Project
+
+
+
+Description: This is a simple 3D Roll-a-Ball game made in Unity. The
+
+player controls a ball. The camera follows the
+
+player.
+
+
+
+Steps Completed: 1. Setting up the game: - Created ground (Plane) -
+
+Created player (Sphere) - Added walls
+
+
+
+2\.  Moving the Player:
+
+
+
+\-   Added PlayerMovement script
+
+\-  Added character controler
+
+3\.  Moving the Camera:
+
+
+
+\-   Added CameraFollow script
+
+\-   Linked player to camera
+
+
+
+4\.  Creating transition area:
+
+
+
+\- Fence added
+
+
+
+5\.  Assets:
+
+
+
+\-   Added materials and colors
+
+
+
+Problems: - CameraFollow script did not work → fixed by assigning Player
+
+in Inspector - NullReference error → fixed with null check
+
+
+
+Lessons Learned: - Rigidbody movement - Camera follow system - Trigger
+
+&#x20;- GitHub usage with .gitignore
+
+
+
+AI Assistance: Used ChatGPT for help with scripts and debugging.
+
+
+
+How to Run: 1. Open project in Unity 2. Press Play 3. Use WASD or Arrow
+
+keys to move
+
